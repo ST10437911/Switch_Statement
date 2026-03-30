6770252363e89abcf99038056780abf652b82b23
Ziyanda Mpungose <ST10437911@vcconnect.edu.za>
2026-03-30 09:24:35 +0200

    Commit 02: Imported Scanner Object
    The scanner object was imported in order to accept input from the user

--- src/main/java/com/mycompany/switch_statements/Switch_Statements.java
+++ src/main/java/com/mycompany/switch_statements/Switch_Statements.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.switch_statements;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lab_services_student
@@ -11,6 +13,15 @@ package com.mycompany.switch_statements;
 public class Switch_Statements {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        /*
+        Create a Application that demonstrates the diffrence between 
+        Nested If-Else Statements and Switch Statements 
+        
+        Prompt the user to Enter a Number and Based on the Number 
+        Entered, display the day. For example: 2 -- Tuesday
+        */
+        
+        // Import Scanner Object
+        Scanner kb = new Scanner(System.in);
     }
 }
